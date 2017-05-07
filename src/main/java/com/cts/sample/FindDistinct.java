e0aa3b9b95a37bf1257836467670293b1dfe6216
snehasis007 <snehasis007@gmail.com>
2017-05-07 11:24:27 +0530

    batch3

--- src/main/java/com/cts/sample/FindDistinct.java
+++ src/main/java/com/cts/sample/FindDistinct.java
@@ -6,26 +6,26 @@ import java.util.List;
 import java.util.Set;
 
 public class FindDistinct {
-	public Set<String> distinctList(String data){
-		String [] args=data.split("\\s");
-		Set<String> dataSet=new HashSet<>();
-		for(String parts:args){
+	public Set<String> distinctList(String data) {
+		String[] args = data.split("\\s");
+		Set<String> dataSet = new HashSet<>();
+		for (String parts : args) {
 			dataSet.add(parts);
 		}
 		return dataSet;
 	}
-	
-	public static void main(String[] args){
-		IOTest1 ioTest1=new IOTest1();
-		FindDistinct fs=new FindDistinct();
+
+	public static void main(String[] args) {
+		IOTest1 ioTest1 = new IOTest1();
+		FindDistinct fs = new FindDistinct();
 		try {
-			List<String> data=ioTest1.load();
-			Set<String> vals=null;
-			for(String line:data){
-				vals=fs.distinctList(line);
-				System.out.println(vals.size()+"distinct words:"+vals);
+			List<String> data = ioTest1.load();
+			Set<String> vals = null;
+			for (String line : data) {
+				vals = fs.distinctList(line);
+				System.out.println(vals.size() + "distinct words:" + vals);
 			}
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
